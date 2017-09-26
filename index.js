d11987ae0e65440c5d52481bb4180f8315a4ce1b
Denisse Urdaneta <denisseurdaneta1999@hotmail.com>
2017-09-26 18:03:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 // write your code below!
 var name = "Susan"
-var height = 74
-if (height==="74"){
-  console.log(The height is correct)
-}
+var height = 39
 var message = ` ${name} is ${height} inches tall`
 
 module.exports = {name, height, message}
